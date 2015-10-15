8f1839779be60d479511dd44703a3dfd14bcd1d0
steven <thedemonsspeed@gmail.com>
2015-10-14 23:35:42 -0500

    Agregada vista de login, registro y lista de productos clientes

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('productosCtrl', function($scope) {
+
+})
+   
+.controller('registrateCtrl', function($scope) {
+
+})
+ 
