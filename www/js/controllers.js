0515da5fb2c64fd3465fcbef23e4ef983ee355da
@phipex <phipex@yahoo.com>
2015-10-15 00:55:24 -0500

    -agregado servicio de productos con json

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,9 +4,14 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('productosCtrl', function($scope) {
+.controller('productosCtrl', ['$scope','ProductosService',function($scope,ProductosService) {
+	$scope.productos = [];
 
-})
+	ProductosService.getAll().then(function(response){
+		console.info(response.data);
+		$scope.productos = response.data		
+	});
+}])
    
 .controller('registrateCtrl', function($scope) {
 
---
+++ www/js/productos.service.js
@@ -0,0 +1,9 @@
+angular.module('app.services', [])
+.service('ProductosService', ['$http',function($http){
+	this.getAll=function (usuario,) {
+
+            return $http.get('data/Productos.json');
+
+
+        };
+}]);
