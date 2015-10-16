5834b10971bba3966e846b2ab900a36342217f1d
steven <thedemonsspeed@gmail.com>
2015-10-15 22:51:34 -0500

    Arreglado erro con una coma, cambio de colores de los botones

--- www/js/productos.service.js
+++ www/js/productos.service.js
@@ -1,6 +1,6 @@
 angular.module('app.services', [])
 .service('ProductosService', ['$http',function($http){
-	this.getAll=function (usuario,) {
+	this.getAll=function (usuario) {
 
             return $http.get('data/Productos.json');
 
