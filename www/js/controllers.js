cf7616516fce3b457067590bad3aee8a3db733d4
@phipex <phipex@yahoo.com>
2015-12-09 00:39:30 -0500

    feature: agragado mapa de google

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -88,7 +88,54 @@ angular.module('app.controllers', [])
             };
 
 	       
+})
+
+.controller('google', function ($scope,$cordovaGeolocation) {
+   
+
+
+  $scope.Nombre = "Andres Felipe Montoya";
+  $scope.Direccion = "Calle 102 No. 64 -82";
+  $scope.telefono = "12345567";
+
+  $scope.windowOptions = {
+            visible: false
+        };
+
+  $scope.map = { center: { latitude: 5, longitude: -73 }, zoom: 5 };
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+  $scope.marker = { 
+    coords: {
+      latitude: 5, longitude: -73
+    },
+    id:1,
+    show: false,
+    options: { draggable: false }
+  }          
+  
+  $scope.callGeolocation = function  (argument) {
+    $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      $scope.map.center.latitude  = position.coords.latitude;
+      $scope.map.center.longitude = position.coords.longitude;
+      $scope.marker.coords.latitude = position.coords.latitude;
+      $scope.marker.coords.longitude = position.coords.longitude;
+      $scope.map.zoom=15;
+      $scope.windowOptions.visible=true;
+      $scope.marker.show = true;
+    }, function(err) {
+      // error
+    });
+
+    
+  }
+
+  $scope.callGeolocation();
 });
+
+
 
 
 
